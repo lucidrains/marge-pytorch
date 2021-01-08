bb3530e4718c689f592f84acb2d7bfe6ffd1c7f8
Phil Wang <lucidrains@gmail.com>
2021-01-08 10:42:34 -0800

    0.2.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'marge-pytorch',
   packages = find_packages(),
-  version = '0.2.5',
+  version = '0.2.7',
   license='MIT',
   description = 'Marge - Pytorch',
   author = 'Phil Wang',
